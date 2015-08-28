aff5323da603d845e499cbd851e267171d1b4bff
Casey Koellner <koellner.casey73@gmail.com>
2015-08-28 01:31:11 -0400

    Player can now win the game under the proper conditions

--- hangman.rb
+++ hangman.rb
@@ -27,16 +27,25 @@ class Hangman
 
 	def Hangman.display_board
 		print("\n")
-		print(@@guesses.join(", ") + "\n")
+		print(@@word)
+		print("\n")
+		print(@@guesses.join(", ") + "\n\n")
 
+		count = 0
 		(@@wordLength - 1).times do
-			print(@@board[0] + " ")
+			print(@@board[count] + " ")
+			count += 1
 		end
+		print("\n\n")
 	end
 
 	#Checks whether the player input is an alphabetical character
-	def Hangman.check_input
+	def Hangman.input
 		guess = gets.chomp
+		while(guess.length != 1)
+			print("Please enter a letter: ")
+			guess = gets.chomp
+		end
 		while(guess.to_s == guess.to_i.to_s)
 			print("Please enter a letter: ")
 			guess = gets.chomp
@@ -49,21 +58,56 @@ class Hangman
 		return guess
 	end
 
+	def Hangman.win
+		count = 0
+		@@word.split("").each do |x|
+			if(x == @@board[count])
+				count += 1
+			end
+		end
+		if(count == (@@word.length - 1))
+			return true
+		else return false
+		end
+	end
+
 	def play
 		playerGuess = ""
 
 		numGuesses = 0
-
 		Hangman.display_board
 
+		x = ""
 		while numGuesses < 6
 			print("\n")
 			Hangman.display_board
 
 			print("Guess a letter: ")
-			playerGuess = Hangman.check_input
-			@@guesses << playerGuess
-			numGuesses += 1
+			playerGuess = Hangman.input
+			
+			count = 0			
+			#Iterates through each character in the random word
+			@@word.split("").each_with_index do |character,index|
+				if playerGuess == character
+					@@board[index] = playerGuess
+					count += 1
+				else 
+					next
+				end
+			end
+
+			if count == 0
+				numGuesses += 1
+			end
+
+			#print("\n\n#{@@board.join("")}\n\n")
+
+			if Hangman.win
+				print("You have won!")
+				break
+			end
+
+			@@guesses << playerGuess	
 		end
 
 		print("\n")
